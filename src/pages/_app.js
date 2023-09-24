e7df6f221ddaf242e37829105f37f0924ece0bae
sifatullahsu <personal.sifat@gmail.com>
2023-09-24 06:56:34 +0600

    mainlayout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,23 @@
 import '@/styles/globals.css'
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: ['400', '500', '700'],
+  variable: '--font-roboto',
+  subsets: ['latin']
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout ?? (page => page)
+
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --font-roboto: ${roboto.style.fontFamily};
+        }
+      `}</style>
+      {getLayout(<Component {...pageProps} />)}
+    </>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
+    fontFamily: {
+      roboto: ['var(--font-roboto)']
+    },
     container: {
       center: true,
       padding: {
