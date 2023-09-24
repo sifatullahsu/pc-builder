b332c45f7896bca33d8918479772508a93e48bf8
sifatullahsu <personal.sifat@gmail.com>
2023-09-24 16:17:39 +0600

    header footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,34 @@
+import Link from 'next/link'
+import { BiLogoFacebook, BiLogoFirebase, BiLogoInstagram, BiLogoYoutube } from 'react-icons/bi'
+
 const Footer = () => {
   return (
-    <div>
-      <div>Footer</div>
-    </div>
+    <section className="border-t border-b">
+      <div className="container">
+        <footer className="footer grid-cols-3 py-4 px-2">
+          <aside className="items-center grid-flow-col col-span-2">
+            <BiLogoFirebase fontSize={30} />
+            <p>
+              PC Builder Ltd. <br />
+              Providing service since 1992
+            </p>
+          </aside>
+          <nav className="place-self-center justify-self-end">
+            <div className="grid grid-flow-col gap-4 text-xl">
+              <Link href="/">
+                <BiLogoFacebook />
+              </Link>
+              <Link href="/">
+                <BiLogoYoutube />
+              </Link>
+              <Link href="/">
+                <BiLogoInstagram />
+              </Link>
+            </div>
+          </nav>
+        </footer>
+      </div>
+    </section>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,75 @@
-import { signIn, signOut, useSession } from 'next-auth/react'
+import { useSession } from 'next-auth/react'
+import Link from 'next/link'
+import { BiLogoFirebase } from 'react-icons/bi'
+import { HiBars3BottomLeft } from 'react-icons/hi2'
 
 const Header = () => {
   const { data: session } = useSession()
   console.log(session)
 
+  const items = () => {
+    return (
+      <>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li tabIndex={0}>
+          <details>
+            <summary>Categories</summary>
+            <ul className="p-2">
+              <li>
+                <Link href="/">Processor</Link>
+              </li>
+              <li>
+                <Link href="/">Motherboard</Link>
+              </li>
+              <li>
+                <Link href="/">RAM</Link>
+              </li>
+              <li>
+                <Link href="/">Power Supply Unit</Link>
+              </li>
+              <li>
+                <Link href="/">SSD</Link>
+              </li>
+              <li>
+                <Link href="/">Monitor</Link>
+              </li>
+            </ul>
+          </details>
+        </li>
+        <li>
+          <Link href="/">PC Builder</Link>
+        </li>
+      </>
+    )
+  }
+
   return (
-    <div>
-      <div>Header</div>
-      <button onClick={() => signIn()}>Sign in</button>
-      <button onClick={() => signOut()}>Sign out</button>
-    </div>
+    <section className="border-b">
+      <div className="container">
+        {/* <button onClick={() => signIn()}>Sign in</button>
+      <button onClick={() => signOut()}>Sign out</button> */}
+
+        <div className="navbar">
+          <div className="md:navbar-start ">
+            <div className="dropdown">
+              <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                <HiBars3BottomLeft fontSize={22} />
+              </label>
+              <ul className="menu menu-sm dropdown-content shadow bg-base-100">{items()}</ul>
+            </div>
+            <Link href="/" className="text-xl">
+              <BiLogoFirebase className="inline text-[30px]" />
+              PC BUILDER
+            </Link>
+          </div>
+          <div className="navbar-end hidden md:block text-right">
+            <ul className="menu menu-horizontal px-1 -mr-5">{items()}</ul>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
